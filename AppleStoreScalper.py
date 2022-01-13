e5e4fd8691c8a6603a03a419983517b8f4f6c4d8
Philippe Geukers <philippe.geukers.1@ens.etsmtl.ca>
2022-01-13 12:25:45 -0500

    Added silver macbook

--- AppleStoreScalper.py
+++ AppleStoreScalper.py
@@ -10,8 +10,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-# URL = "https://www.apple.com/ca_edu_93120/shop/buy-mac/macbook-pro/14-inch-space-grey-8-core-cpu-14-core-gpu-512gb#"
-URL = "https://www.apple.com/ca/shop/buy-mac/macbook-air/space-grey-apple-m1-chip-with-8-core-cpu-and-7-core-gpu-256gb#"
+URLs = {"Space Grey": "https://www.apple.com/ca/shop/buy-mac/macbook-pro/14-inch-space-grey-8-core-cpu-14-core-gpu-512gb#",
+        "Silver": "https://www.apple.com/ca/shop/buy-mac/macbook-pro/14-inch-silver-8-core-cpu-14-core-gpu-512gb#"}
 
 
 def send_email(text):
@@ -22,7 +22,7 @@ def send_email(text):
     FROM = SMTP_LOGIN
     TO = ["philippe.geukers.1@ens.etsmtl.ca"]  # must be a list
     SUBJECT = 'The Macbook Pro is available'
-    message = text + "Order now : " + URL
+    message = text + "Order now : " + URLs
 
     # Prepare actual message
     msg = EmailMessage()
@@ -40,37 +40,39 @@ def send_email(text):
 
 if __name__ == '__main__':
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    while True:
+    notFound = True
+    while notFound:
         # p = Path(__file__).with_name('chromedriver.exe')
         # print(p)
         # driver = webdriver.Chrome(p)
-        driver.get(URL)
+        for NAME, URL in URLs.items():
+            driver.get(URL)
 
-        time.sleep(1)
+            time.sleep(1)
 
-        driver.find_element(by=By.CLASS_NAME, value="as-retailavailabilitytrigger-infobutton").click()
-        inputElement = driver.find_element(by=By.ID, value="as-retailavailabilitysearch-query")
-        inputElement.send_keys('J4Y0L3')
-        driver.find_element(by=By.CLASS_NAME, value="search-stores").click()
+            driver.find_element(by=By.CLASS_NAME, value="as-retailavailabilitytrigger-infobutton").click()
+            inputElement = driver.find_element(by=By.ID, value="as-retailavailabilitysearch-query")
+            inputElement.send_keys('J4Y0L3')
+            driver.find_element(by=By.CLASS_NAME, value="search-stores").click()
 
-        time.sleep(1)
+            time.sleep(1)
 
-        stores = driver.find_elements(by=By.CLASS_NAME, value="as-storeitem")
-        available_stores = []
-        for i in range(4):
-            if "Available" in stores[i].text:
-                available_stores.append(stores[i].find_element(by=By.CLASS_NAME, value="as-storeitem-storename").text)
+            stores = driver.find_elements(by=By.CLASS_NAME, value="as-storeitem")
+            available_stores = []
+            for i in range(4):
+                if "Available" in stores[i].text:
+                    available_stores.append(stores[i].find_element(by=By.CLASS_NAME, value="as-storeitem-storename").text)
 
-        if len(available_stores) > 0:
-            text = ""
-            for store in available_stores:
-                text += store + "\n"
-            print(str(datetime.datetime.now()))
-            print(text)
-            send_email(text)
-            driver.quit()
-            break
-        print(str(datetime.datetime.now()) + " : No available MacBooks")
-        time.sleep(10)
+            if len(available_stores) > 0:
+                text = ""
+                for store in available_stores:
+                    text += NAME + " : " + store + "\n"
+                print(str(datetime.datetime.now()))
+                print(text)
+                send_email(text)
+                notFound = False
+                break
+            print(str(datetime.datetime.now()) + " : No available MacBooks - " + NAME)
+        time.sleep(60 * 5)
     driver.quit()
 
